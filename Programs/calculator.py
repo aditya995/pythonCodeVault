796c3d099391651128fec7afbb2b2e6fb65be83e
Aditya Showrov <adityashowrov1996@gmail.com>
2022-06-14 05:24:07 +0600

    evaluation fixed, power added

--- Programs/calculator.py
+++ Programs/calculator.py
@@ -28,11 +28,11 @@ def createTokens(inputExpr: str):
     tokens = []
     token = ''
     for c in inputExpr:
-        if c.isnumeric():
+        if c.isnumeric() or c == '.':
             token += c
-        elif c == '+' or c == '-' or c == '*' or c == '/' or c == '(' or c == ')' or c == ' ':
+        elif c == '+' or c == '-' or c == '*' or c == '/' or c == '(' or c == ')' or c == ' ' or c == '^':
             if token != '':
-                tokens.append(int(token))
+                tokens.append(float(token))
             if c != ' ':
                 tokens.append(c)
             token = ''
@@ -40,9 +40,9 @@ def createTokens(inputExpr: str):
             return ['*****   Invalid expression   *****']
     # formatting 1(2)3 as 1*(2)*3, putting '*' where necessary
     for i in range(1, len(tokens)):
-        if i+1 < len(tokens) and type(tokens[i+1]) == type(1) and tokens[i] == ')':
+        if i+1 < len(tokens) and type(tokens[i+1]) == type(1.0) and tokens[i] == ')':
             tokens.insert(i+1, '*')
-        elif tokens[i] == '(' and type(tokens[i-1]) == type(1):
+        elif tokens[i] == '(' and type(tokens[i-1]) == type(1.0):
             tokens.insert(i, '*')
         elif tokens[i] == '(' and tokens[i-1] == ')':
             tokens.insert(i, '*')
@@ -53,70 +53,68 @@ def convertToPostfix(tokens):
     postfix = []
     stack = []
     operatorPriority = {
+        '^': 6,
         '/': 4,
         '*': 4,
         '+': 2,
         '-': 2,
     }
     for i in tokens:
-        if type(i) == type(1):
+        if type(i) == type(1.0):
             postfix.append(i)
-        else:
-            if i == '(':
+        elif i == '(':
+            stack.append(i)
+        elif i == ')':
+            itCount = len(stack)
+            for op in range(0, itCount):
+                if stack[-1] != '(':
+                    postfix.append(stack.pop())
+                else:
+                    break
+            stack.pop()
+        elif i == '+' or i == '-' or i == '*' or i == '/' or i == '^':
+            if not stack.count and stack[-1] != '(' and operatorPriority[stack[-1]] < operatorPriority[i]:
                 stack.append(i)
-            elif i == ')':
+            elif len(stack) != 0 and stack[-1] != '(' and operatorPriority[stack[-1]] > operatorPriority[i]:
                 itCount = len(stack)
                 for op in range(0, itCount):
                     if stack[-1] != '(':
                         postfix.append(stack.pop())
                     else:
                         break
-                stack.pop()
-            elif i == '+' or i == '-' or i == '*' or i == '/':
-                if not stack.count and (stack[-1] == '(' or operatorPriority[stack[-1]] < operatorPriority[i]):
-                    stack.append(i)
-                elif len(stack) != 0 and stack[-1] != '(' and operatorPriority[stack[-1]] > operatorPriority[i]:
-                    itCount = len(stack)
-                    for op in range(0, itCount):
-                        if stack[-1] != '(':
-                            postfix.append(stack.pop())
-                    stack.append(i)
-                else:
-                    stack.append(i)
-        # input() # debug
-        # show(stack) # debug
-        # show(postfix) # debug
+                stack.append(i)
+            elif not stack.count and (stack[-1] != '(' and operatorPriority[stack[-1]] == operatorPriority[i]):
+                postfix.append(stack.pop())
+                stack.append(i)
+            else:
+                stack.append(i)
+        # input()  # debug
+        show(stack)  # debug
+        show(postfix)  # debug
     return postfix
 
 
 def evaluatePostfix(postfixTokens):
     if len(postfixTokens) < 3:
         return postfixTokens[0]
-    i = 2
-    while i and len(postfixTokens) >= 3:
-        if type(postfixTokens[i-2]) == type(1) and type(postfixTokens[i-1]) == type(1) and not type(postfixTokens[i]) == type(1):
-            show(postfixTokens, False, 'red', [i-1, i-2, i])  # debug
-            a = postfixTokens[i-2]
-            b = postfixTokens[i-1]
-            c = postfixTokens[i]
-            result = 0
-            if c == '+':
-                result = a+b
-            elif c == '-':
-                result = a-b
-            elif c == '*':
-                result = a*b
-            elif c == '/':
-                result = a/b
-            postfixTokens[i-2] = result
-            postfixTokens.pop(i-1)
-            postfixTokens.pop(i-1)
-            show(postfixTokens, False, 'green', [i-2])  # debug
-            print()
-            i = 2
+    cStack = []
+    for i in postfixTokens:
+        if type(i) == type(1.0):
+            cStack.append(i)
         else:
-            i += 1
-    return postfixTokens[0]
+            if i == '+':
+                result = cStack[-2]+cStack[-1]
+            elif i == '-':
+                result = cStack[-2]-cStack[-1]
+            elif i == '*':
+                result = cStack[-2]*cStack[-1]
+            elif i == '/':
+                result = cStack[-2]/cStack[-1]
+            elif i == '^':
+                result = cStack[-2]**cStack[-1]
+            cStack.pop()
+            cStack[-1] = result
+    return cStack[0]
 
 # main function
 
@@ -144,8 +142,8 @@ while(True):
         result = evaluatePostfix(postfixTokens)
     except ValueError as ex:
         print(ex)
-    except Exception:
-        print('*****   Invalid syntax   *****')
+    except Exception as ex:
+        print(ex, '*****   Invalid syntax   *****')
     else:
-        print(result)
+        print(result, 1*(3-4)*5+5*2*(1+2))
 print('Thanks for using my program 😍')
--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-# iterables
-# iterators
-# iteration
+print(float(1))
+print(type(1.0) is type(1.0))
